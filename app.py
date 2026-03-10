80fcb083ba5d1986eb7f4a6bf1f52e34d3584a32
MustafaKhodary <eng.elkhodry1@gmail.com>
2026-03-10 23:56:16 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,10 +6,11 @@ st.write("اكتب فكرة بسيطة وسأقوم بتحويلها إلى قص
  with st.sidebar:
 st.header("⚙️ الإعدادات")
 api_key = st.text_input("أدخل مفتاح Gemini API الخاص بك", type="password")
-st.info("هذا التطبيق يستخدم ذكاء Gemini.")
+st.info("قم بلصق مفتاحك هنا")
  if api_key:
-try:
-genai.configure(api_key= api_key)
-else:
+ try:
+genai.configure(api_key=api_key)
+model = genai.GenerativeModel('gemini-1.5-flash')
+ else:
 st.warning("⚠️ يرجى إدخال مفتاح الـ API في القائمة الجانبية.")
 
