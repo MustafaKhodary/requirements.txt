d2e8ced8e71805007bdda120f6965b12b977ecdf
MustafaKhodary <eng.elkhodry1@gmail.com>
2026-03-10 23:51:42 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ import google.generativeai as genai
 st.set_page_config(page_title="حكايات الصغار الذكية", page_icon="🌙")
 st.markdown("<h1 style='text-align: center; color: #4A90E2;'>🌙 مؤلف قصص الأطفال</h1>", unsafe_allow_html=True)
 st.write("اكتب فكرة بسيطة وسأقوم بتحويلها إلى قصة تربوية ممتعة!")
-with st.sidebar:
+ with st.sidebar:
 st.header("⚙️ الإعدادات")
 api_key = st.text_input("أدخل مفتاح Gemini API الخاص بك", type="password")
 st.info("هذا التطبيق يستخدم ذكاء Gemini.")
-if api_key:
+ if api_key:
 try:
 genai.configure(api_key= api_key)
 else:
